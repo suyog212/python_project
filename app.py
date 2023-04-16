5f1d359754f70fb7c007b8b4c410f772ac0d3f5b
suyog212 <130860821+suyog212@users.noreply.github.com>
2023-04-16 07:01:39 +0000

    Documented some code

--- app.py
+++ app.py
@@ -3,26 +3,31 @@ import requests
 from geopy.geocoders import Nominatim
 import json
 
+#getting longitude and latitude
 
-def get_long_lat(city_name):
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    location=geolocator.geocode(city_name)
-    return [location.longitude,location.latitude]
 
-BASE_URL = "http://api.weatherapi.com/v1/history.json?key=&q=London&dt=2010-01-01"
-API_KEY = "f9955666136632343aff2e4ca9d9c909"
-CITY = "London"
+def get_weather(city_Name):
+    def get_long_lat(city_name):
+        geolocator = Nominatim(user_agent="Project")
+        location=geolocator.geocode(city_name)
+        return [location.longitude,location.latitude]
+    
+    lat_long = get_long_lat(city_Name)
+    #API key
+    API_KEY = "f9955666136632343aff2e4ca9d9c909"
+    #API url to fetch data
+    BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?lat="
+    CITY = "London"
+    url = BASE_URL + lat_long[0] + "&lon="+ lat_long[1] + "&exclude=minutely&appid=" + API_KEY
+    #request object to fetch data from api
+    # res = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat_long[0]}&lon={lat_long[1]}&exclude=minutely&appid={API_KEY}").json()4
+    res = requests.get(url)
+    #Creating and adding data to JSON file 
+    with open("data.json","w") as file:
+        json.dump(res,file)
 
-url = BASE_URL + "appid=" + API_KEY + "&q=" + CITY
+    file.close()
 
-res = requests.get(f"https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely&appid={API_KEY}").json()
 
-# js_data = json.dumps(res)
-
-with open("data.json","w") as file:
-    json.dump(res,file)
-
-file.close()
-
+get_weather("London")
 print("Done !!!!!")
-print(get_long_lat("London")[0])
